b44b2614cbd4fb02afb618a7c623c9c869f6056f
Diogo Martins de Assis <diogomartinsdeassis1@gmail.com>
2023-05-12 14:57:30 -0300

    Configurando o MongoDB

---
+++ Catalog.API/Data/CatalogContextSeed.cs
@@ -0,0 +1,26 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool existsProduct = productCollection.Find(p => true).Any();
+
+            if(!existsProduct)
+            {
+                productCollection.InsertManyAsync(GetMyProducts());
+            }
+        }
+
+        public static IEnumerable<Product> GetMyProducts()
+        {
+            return new List<Product>()
+            {
+
+            };
+        }
+    }
+}
