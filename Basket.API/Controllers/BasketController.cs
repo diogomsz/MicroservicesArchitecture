2d9d2790b31ce89942aa6d2310d4147a43b75546
Diogo Martins de Assis <diogomartinsdeassis1@gmail.com>
2023-05-22 21:19:53 -0300

    Criando repositório e controller

---
+++ Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,70 @@
+﻿using Basket.API.Entities;
+using Basket.API.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.API.Controllers;
+
+[Route("api/b1/[controller]")]
+[ApiController]
+public class BasketController : ControllerBase
+{
+    private readonly IBasketRepository _basketRepository;
+    /// <summary>
+    ///     Injeção de dependência
+    /// </summary>  
+    /// <param name="basketRepository">
+    ///     Serviço do repositório
+    /// </param>
+    public BasketController(IBasketRepository basketRepository)
+    {
+        _basketRepository = basketRepository ?? throw new ArgumentNullException(nameof(basketRepository));
+    }
+
+    /// <summary>
+    ///     Obtendo a cesta de um usuário
+    /// </summary>
+    /// <param name="userName">
+    ///     Nome do usuário
+    /// </param>
+    /// <returns>
+    ///     Retorna a cesta do usuário, caso seja o primeiro
+    ///     acesso do mesmo, retorna uma nova cesta
+    /// </returns>
+    [HttpGet("{userName}", Name = "GetBasket")]
+    public async Task<ActionResult<ShoppingCart>> GetBasket(string userName)
+    {
+        var basket = await _basketRepository.GetBasket(userName);
+        return Ok(basket ?? new ShoppingCart(userName));
+    }
+
+    /// <summary>
+    ///     Atualizando a cesta
+    /// </summary>
+    /// <param name="basket">
+    ///     Cesta do usuário
+    /// </param>
+    /// <returns>
+    ///     Retorna a nova cesta do usuário
+    /// </returns>
+    [HttpPost]
+    public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
+    {
+        return Ok(await _basketRepository.UpdateBasket(basket));
+    }
+
+    /// <summary>
+    ///     Apagando uma cesta
+    /// </summary>
+    /// <param name="userName">
+    ///     Nome do usuário
+    /// </param>
+    /// <returns>
+    ///     Retorna status 200 de deletado com sucesso
+    /// </returns>
+    [HttpDelete("{userName}", Name = "DeleteBasket")]
+    public async Task<IActionResult> DeleteBasket(string userName)
+    {
+        await _basketRepository.DeleteBasket(userName);
+        return Ok();
+    }
+}
---
+++ Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,69 @@
+﻿using Basket.API.Entities;
+using Basket.API.Repositories.Interfaces;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace Basket.API.Repositories;
+
+public class BasketRepository : IBasketRepository
+{
+    private readonly IDistributedCache _redisCache;
+    /// <summary>
+    ///     Injeção de Dependência
+    /// </summary>
+    /// <param name="redisCache">
+    ///     Parâmetro que representa o Cache Redis
+    /// </param>
+    public BasketRepository(IDistributedCache redisCache)
+    {
+        _redisCache = redisCache ?? throw new ArgumentNullException(nameof(redisCache));
+    }
+
+    /// <summary>
+    ///     Obtendo uma cesta
+    /// </summary>
+    /// <param name="userName">
+    ///     Nome do usuário
+    /// </param>
+    /// <returns>
+    ///     Retorna um carrinho de compras
+    /// </returns>
+    public async Task<ShoppingCart?> GetBasket(string userName)
+    {
+        var basket = await _redisCache.GetStringAsync(userName);
+
+        if(string.IsNullOrEmpty(basket))
+        {
+            return null;
+        }
+
+        return JsonSerializer.Deserialize<ShoppingCart?>(basket);
+    }
+
+    /// <summary>
+    ///     Atualizando uma cesta
+    /// </summary>
+    /// <param name="basket">
+    ///     Cesta atualizada
+    /// </param>
+    /// <returns>
+    ///     Retorna um carrinho de compras
+    /// </returns>
+    public async Task<ShoppingCart?> UpdateBasket(ShoppingCart basket)
+    {
+        await _redisCache.SetStringAsync(basket.UserName, JsonSerializer.Serialize(basket));
+        return await GetBasket(basket.UserName);
+    }
+
+    /// <summary>
+    ///     Apaga uma cesta
+    /// </summary>
+    /// <param name="userName">
+    ///     Nome do usuário
+    /// </param>
+    public async Task DeleteBasket(string userName)
+    {
+        await _redisCache.RemoveAsync(userName);
+    }
+
+}
