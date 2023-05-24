96e5e5c3ae9c7ae43537fc5164e5433c51d7109a
Diogo Martins de Assis <diogomartinsdeassis1@gmail.com>
2023-05-24 17:39:55 -0300

    Criando repositories

---
+++ Discount.API/Controllers/DiscountController.cs
@@ -0,0 +1,21 @@
+﻿using Discount.API.Repository.Interfaces;
+
+namespace Discount.API.Controllers;
+
+public class DiscountController
+{
+    private readonly IDiscountRepository _discountRepository;
+    /// <summary>
+    ///     Injeção de Dependência
+    /// </summary>
+    /// <param name="discountRepository">
+    ///     Com este parâmetro podemos usar os métodos
+    ///     para comunicarmos com o banco de dados usando padrão Repository
+    /// </param>
+    public DiscountController(IDiscountRepository discountRepository)
+    {
+        _discountRepository = discountRepository;
+    }
+
+    // PAREI AQUI //////////////////////////////////
+}
