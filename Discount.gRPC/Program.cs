c08e35bcdbc4caeb88db5e2600727a90e4b1a5fd
Diogo Martins de Assis <diogomartinsdeassis1@gmail.com>
2023-05-27 16:00:00 -0300

    Criando mensagens com GRPC

--- Discount.gRPC/Program.cs
+++ Discount.gRPC/Program.cs
@@ -1,4 +1,5 @@
-using Discount.gRPC.Services;
+using Discount.gRPC.Repositories;
+using Discount.gRPC.Repository.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,10 +9,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddGrpc();
 
+// Add Repository
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+// app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
